1013dc1d5036d5ebec18ff3d06b5f95ce47d31b5
Raunak Kumar <nodepoint3@gmail.com>
2026-03-21 13:57:34 +0530

    Added Theme Toggle and Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter, Space_Grotesk } from "next/font/google";
+import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import Navbar from "@/components/navigation/navbar";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -16,6 +19,9 @@ export const metadata: Metadata = {
   title: "WebFlow",
   description:
     "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+  icons: {
+    icon: "/images/site-logo.svg",
+  },
 };
 
 export default function RootLayout({
@@ -27,8 +33,20 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
+      suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Navbar/>
+            {children}
+          </ThemeProvider>
+        <Toaster position="top-right" richColors />
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='flex items-center justify-center min-h-screen h1-bold'>page <span className='text-orange-950 h1-bold'>orange</span></div>
+    <div>page</div>
   )
 }
 
